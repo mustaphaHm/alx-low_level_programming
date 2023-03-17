1a49821a7f827818fcd6c1219e02794329a4caa2
MustaphaHm <mustapha.houmam@gmail.com>
2023-03-17 15:11:55 -0700

    first task in c seconde session

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,27 +2,26 @@
 #include <time.h>
 #include <stdio.h>
 /**
-*main - principale function
-*Return : return always (0)
-*/
+ *main - Entry point
+ *Return: always 0
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if (n < 0) 
+	else if (n == 0)
 	{
-		printf("%d is negative\n", n);
+		printf("%d is zero\n, n");
 	}
 	else
 	{
-		printf("%d is zero\n", n);
+		printf("%d is negative\n, n");
 	}
 	return (0);
 }
