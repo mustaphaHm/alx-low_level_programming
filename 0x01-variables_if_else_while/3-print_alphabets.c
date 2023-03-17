e2444e43bd8871fdaa91b5ea0b0a0e8432b0dcea
MustaphaHm <mustapha.houmam@gmail.com>
2023-03-17 16:19:06 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	for (l = 'A'; l <= 'Z'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+
+	return (0);
+}
