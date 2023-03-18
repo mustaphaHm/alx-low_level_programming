6d1f933b1fdaf3b6125c4caf6cb153882b3d32aa
MustaphaHm <mustapha.houmam@gmail.com>
2023-03-17 17:13:19 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'z'; n <= 'a'; n--)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
