454ce8f4ccae8b9cf576dba354147a5b261019c4
MustaphaHm <mustapha.houmam@gmail.com>
2023-03-23 17:16:28 -0700

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a straight line
+ *
+ * @n: number of time the _ should be printed
+ */
+void print_diagonal(int n)
+{
+	int lenght, spaces;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (lenght = 1; lenght <= n; lenght++)
+		{
+			for (spaces = 0; spaces <= lenght; spaces++)
+				_putchar(' ');
+			_putchar('\\');
+		}
+		_putchar('\n');
+	}
+}
