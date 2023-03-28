7b3e10f2e9969fc9d883d3bb5c3f39871255317d
MustaphaHm <mustapha.houmam@gmail.com>
2023-03-27 17:15:08 -0700

    reverse a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - print a string in revrse
+ *
+ * @s: parametre de type char
+ * Return: Always 0
+ */
+void print_rev(char *s)
+{
+	int lenght = 0;
+	int y;
+
+	while (*s != '\0')
+	{
+		lenght++;
+	}
+	for (y = lenght; y >= 0; y--)
+	{
+		_putchar(s[y]);
+	}
+	_putchar('\n');
+}
