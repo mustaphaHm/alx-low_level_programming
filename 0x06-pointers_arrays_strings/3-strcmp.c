c498a7d1064d661207e8d7e35df5b58fd1f4af5b
MustaphaHm <mustapha.houmam@gmail.com>
2023-03-30 17:13:05 -0700

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,7 +12,7 @@ int _strcmp(char *s1, char *s2)
 
 	for (count = 0; s2[count] != '\0'; count++)
 		if (s1[count] != s2[count])
-			return (s1[count] - s2[count]); 
+			return (s1[count] - s2[count]);
 	return (0);
 
 }
