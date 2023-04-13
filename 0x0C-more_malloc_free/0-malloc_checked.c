d17037cb5a8f6cb739ca034f497525c979e2ae9b
MustaphaHm <mustapha.houmam@gmail.com>
2023-04-13 16:01:21 -0700

    a function that allocates memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,10 +7,12 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	int *ptr;
+	void *ptr;
 
-	ptr = malloc(b * sizeof(*ptr));
+	ptr = malloc(b);
 	if (ptr == NULL)
+	{
 		exit(98);
+	}
 	return (ptr);
 }
