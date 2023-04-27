937afd52bf7473b691d91ea37c8ec633986d54ab
MustaphaHm <mustapha.houmam@gmail.com>
2023-04-27 15:32:21 -0700

    a function that adds a new node at the beginning of a list_t list.

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -19,6 +20,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+int LenghtOfStr(char *s);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
