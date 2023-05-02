08ad41ba515750637cb2f9d9f9be705131becdb7
MustaphaHm <mustapha.houmam@gmail.com>
2023-05-02 15:46:26 -0700

    return the sum of the all data of a linekd list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * sum_listint - function that returns the sum of all data in a linked list
+ * @head: the pointer to the linked list
+ * Return: int
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *first;
+	int sum = 0;
+	
+	if (!head)
+		return (0);
+	first = head;
+	while (first)
+	{
+		sum = sum + first->n;
+		first = first->next;
+	}
+	return (sum);
+}
