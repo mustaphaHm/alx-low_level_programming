07d77037ce147f25d87cb733cb711f3bf5c3c216
MustaphaHm <mustapha.houmam@gmail.com>
2023-05-02 14:26:15 -0700

    free a linked list 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - function that free a linked list
+ * @head: pointer to a pointer of a linked list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *startElm, *nextElm;
+
+	if (!head)
+		return;
+	startElm = *head;
+	while (startElm)
+	{
+		nextElm = startElm->next;
+		free(startElm);
+		startElm = nextElm;
+	}
+}
