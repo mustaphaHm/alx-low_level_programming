182d75b258ed64a237f181705c41ece23a9f2662
MustaphaHm <mustapha.houmam@gmail.com>
2023-05-09 15:30:48 -0700

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	while (text_content[lenght])
+	while (text_content != NULL && text_content[lenght])
 	{
 		lenght++;
 	}
