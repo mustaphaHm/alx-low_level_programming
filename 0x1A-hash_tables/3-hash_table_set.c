9e5250c02df3de0c39d5eb4570f0258a28767004
MustaphaHm <mustapha.houmam@gmail.com>
2023-07-07 11:54:17 -0700

    Task 3, function that adds an item to a linked list

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,15 +8,17 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t item = NULL;
+	hash_node_t *item, *current_item;
 	unsigned long int index = 0;
+
 
 	/*create the item to be inserted*/
 	item = create_item(key, value);
 	/* retrieve the index by using the key */
-	index = key_index(key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	/* retrive the current item at the founded index*/
-	hash_node_t current_item = ht->array[index];
+	current_item = ht->array[index];
+
 	if (current_item == NULL)
 	{
 		/*insert the item*/
@@ -28,13 +30,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		/*if the key already exist, we gonna update the value*/
 		if (strcmp(current_item->key, key) == 0)
 		{
-			ht->array[index]->value = value;
+			strcpy(ht->array[index]->value, value);
 			return (1);
 		}
 		else
 		{
 			/* handle collision*/
-			handle_collision(ht, index, item);
+			handle_coll(ht, index, item);
 			return (1);
 		}
 	}
@@ -49,6 +51,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 hash_node_t *create_item(const char *key, const char *value)
 {
 	hash_node_t *item = malloc(sizeof(hash_node_t));
+
 	item->key = malloc(strlen(key) + 1);
 	item->value = malloc(strlen(value) + 1);
 
@@ -57,30 +60,30 @@ hash_node_t *create_item(const char *key, const char *value)
 	return (item);
 }
 /**
- * handle-collision - function that hundles collision on a ht
+ * handle_coll- function that hundles collision on a ht
  * @ht: the hashtable
- * @index: the index
- * @item: the item
+ * @idx: the index
+ * @it: the item
+ * Return: void
  */
-void handle_collision(hash_table_t *ht, unsigned long int index, hash_node_t item)
+void handle_coll(hash_table_t *ht, unsigned long int idx, hash_node_t *it)
 {
 	/*retrive the current item at the index*/
-	hash_nod_t current_item = ht->array[index];
+	hash_node_t *current_item = ht->array[idx];
 	/*traverse the linkedlist until the end*/
-	while (current->item->next != NULL)
+	while (current_item->next != NULL)
 	{
 		/* check if the key already exist in the linked list*/
-		if (strcmp(current_item->key, item->key) == 0)
+		if (strcmp(current_item->key, it->key) == 0)
 		{
 			/*the key already exist so update the value*/
-			strcpy(current->item->value, item->value);
-			free(item->key);
-			free(item->value);
-			free(item);
-			return (1);
+			strcpy(current_item->value, it->value);
+			free(it->key);
+			free(it->value);
+			free(it);
 		}
 		current_item = current_item->next;
 	}
 	/* append the item at the end of the linked list*/
-	current_item->next = item;
+	current_item->next = it;
 }
