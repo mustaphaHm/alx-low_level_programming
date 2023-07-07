e67c4954cb06fbfbd84e7f46e1fd58d5776559f3
MustaphaHm <mustapha.houmam@gmail.com>
2023-07-07 14:03:40 -0700

    Task 5, function that prints a hashtable

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -26,7 +26,7 @@ void hash_table_print(const hash_table_t *ht)
 			temp = ht->array[i];
 			while (temp != NULL)
 			{
-				printf("%s: %s", temp->key, temp->value);
+				printf("\'%s\': \'%s\'", temp->key, temp->value);
 				temp = temp->next;
 				if (temp != NULL)
 					printf(", ");
