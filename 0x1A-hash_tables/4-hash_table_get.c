e3a2f32b43e25098dd93dea537a0442f54d09014
MustaphaHm <mustapha.houmam@gmail.com>
2023-07-07 13:36:03 -0700

    Task 4, function that retrieves a value associated with a key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - funct that retries a values by key
+ * @ht: the hashTable
+ * @key: the key
+ * Return: char , the value
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t item;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+	index = key_index(key, ht->size);
+	if (ht->array[index] == NULL)
+		return (NULL);
+	item = ht->array[index];
+	while (item)
+	{
+		if (strcmp(item->key, key) == 0)
+			return (item->value);
+		item = item->next;
+	}
+	return (NULL);
+}
