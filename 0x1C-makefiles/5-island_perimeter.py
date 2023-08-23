06f2514ac1ecf9bdbe85d064f2b4e1a20604a22c
MustaphaHm <mustapha.houmam@gmail.com>
2023-08-23 09:05:55 -0700

    Task 5, Calculat perimetre of a grid

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" module """
+
+
+def island_perimeter(grid):
+    """
+    Calculates the perimeter of the island described in the grid.
+
+    Args:
+    grid (List[List[int]]): A list of lists representing the grid with 0s and 1s.
+
+    Returns:
+    int: The perimeter of the island.
+    """
+    if not grid:
+        return 0
+    
+    rows, cols = len(grid), len(grid[0])
+    perimeter = 0
+    
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4  # Initialize with 4 sides
+
+                # Check left neighbor
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+                # Check upper neighbor
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                    
+    return perimeter
