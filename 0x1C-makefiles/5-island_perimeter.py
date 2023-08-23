ad839cb0d9e88f886e98fd41fa8a98a2642cb956
MustaphaHm <mustapha.houmam@gmail.com>
2023-08-23 09:09:21 -0700

    Task 5, Correct code style and docstring

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-""" module """
+"""module."""
 
 
 def island_perimeter(grid):
     """
-    Calculates the perimeter of the island described in the grid.
+    Calculate the perimeter of the island described in the grid.
 
     Args:
-    grid (List[List[int]]): A list of lists representing the grid with 0s and 1s.
+    grid (List[List[int]]): A list of lists representing the grid.
 
     Returns:
     int: The perimeter of the island.
     """
     if not grid:
         return 0
-    
+
     rows, cols = len(grid), len(grid[0])
     perimeter = 0
-    
+
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
@@ -30,5 +30,5 @@ def island_perimeter(grid):
                 # Check upper neighbor
                 if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
-                    
+
     return perimeter
